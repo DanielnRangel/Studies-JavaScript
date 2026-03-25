da047cd69af7c93810df3d6b89765b33e08e31a2
DanielnRangel <96545079+DanielnRangel@users.noreply.github.com>
2026-03-25 16:01:16 +0000

    aula15

---
+++ aula15/script.js
@@ -0,0 +1,20 @@
+let num = [2, "teste", 6, `${12 * 2}`, 3, 5, 6, 31];
+
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
+
+console.log(num.indexOf(`${12 + 2}`));
+
+// for (let i in num) {
+//   console.log(num[i]);
+// }
+
+//  num[2] = 85;
+
+//  console.log(num.length);
+
+//  num.sort();
+// num.sort(); // Para organizar
+
+// console.log(num);
